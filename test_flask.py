c158fe519cac93d8d8bb2daddf5ba54f2d5b5ecd
Arnav Raj <121608861+deadsmash07@users.noreply.github.com>
2024-01-31 02:33:06 +0530

    eigth mark

--- test_flask.py
+++ test_flask.py
@@ -3,35 +3,14 @@ import yfinance as yf
 from datetime import datetime, timedelta
 import pandas as pd
 import plotly.graph_objects as go
-import plotly.io as pio
-from plotly.utils import PlotlyJSONEncoder
+from plotly.io import to_json
+from plotly.subplots import make_subplots
 import json
+from plotly.utils import PlotlyJSONEncoder
 
 app = Flask(__name__)
 
-@app.route('/home')
-@app.route('/')
-def home():
-    return render_template('index.html')
-
-@app.route('/Explore/', methods=['GET'])
-def explore():
-    return render_template('explore.html')
-
-@app.route('/graph', methods=['GET'])
-def graph():
-    stock_symbol = request.args.get('search')
-
-    if stock_symbol is None:
-        return "No stock symbol provided", 400
-
-    stock_symbol_nse = stock_symbol + ".NS"
-
-    ten_years_ago = datetime.now() - timedelta(days=365 * 10)
-    data = yf.download(stock_symbol_nse, start=ten_years_ago.strftime('%Y-%m-%d'), end=datetime.now().strftime('%Y-%m-%d'))
-
-    data.index = pd.to_datetime(data.index)
-
+def create_line_plot(data, stock_symbol):
     line_plot = go.Figure(data=[go.Scatter(x=data.index, y=data['Close'], name='', hovertemplate='Date: %{x}<br>Close Price: %{y}')])
 
     line_plot.update_layout(
@@ -57,9 +36,74 @@ def graph():
             type="date"
         )
     )
+    
+    return to_json(line_plot)
 
-    fig_json2 = json.dumps(line_plot, cls=PlotlyJSONEncoder)
-    return render_template('graph.html', plot=fig_json2)
+def create_candlestick_plot(data, stock_symbol):
+    candlestick_plot = go.Figure(data=[go.Candlestick(x=data.index,
+                                                      open=data['Open'],
+                                                      high=data['High'],
+                                                      low=data['Low'],
+                                                      close=data['Close'],
+                                                      name='Candlestick',
+                                                      increasing_line_width=1, decreasing_line_width=1,
+                                                      increasing_line_color='blue', decreasing_line_color='red')])
+
+    candlestick_plot.update_layout(
+        title="Candlestick Chart for " + stock_symbol,
+        xaxis_title='Date',
+        yaxis_title='Price',
+        autosize=True,
+        hovermode="x",
+        showlegend=True,
+        xaxis=dict(
+            tickformat='%d %B (%a)<br>%Y',
+            rangeselector=dict(
+                buttons=list([
+                    dict(count=7, label="1W", step="day", stepmode="backward"),
+                    dict(count=1, label="1M", step="month", stepmode="backward"),
+                    dict(count=6, label="6M", step="month", stepmode="backward"),
+                    dict(count=1, label="1Y", step="year", stepmode="backward"),
+                    dict(count=10, label="10Y", step="year", stepmode="todate"),
+                    dict(step="all")
+                ])
+            ),
+            rangeslider=dict(
+                visible=True
+            ),
+            type="date"
+        )
+    )
+
+    return to_json(candlestick_plot)
+
+@app.route('/home')
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/Explore/', methods=['GET'])
+def explore():
+    return render_template('explore.html')
+
+@app.route('/graph', methods=['GET'])
+def graph():
+    stock_symbol = request.args.get('search')
+
+    if stock_symbol is None:
+        return "No stock symbol provided", 400
+
+    stock_symbol_nse = stock_symbol + ".NS"
+
+    ten_years_ago = datetime.now() - timedelta(days=365 * 10)
+    data = yf.download(stock_symbol_nse, start=ten_years_ago.strftime('%Y-%m-%d'), end=datetime.now().strftime('%Y-%m-%d'))
+
+    data.index = pd.to_datetime(data.index)
+
+    line_plot_json = create_line_plot(data, stock_symbol)
+    candlestick_plot_json = create_candlestick_plot(data, stock_symbol)
+
+    return render_template('graph.html', line_plot=line_plot_json, candlestick_plot=candlestick_plot_json)
 
 @app.route('/compare_graph', methods=['GET'])
 def compare_graph():
